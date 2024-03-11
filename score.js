ece08bcffa3aadf1a74a6220e9eefa0c8820f8c7
Saranya2024 <159130151+Saranya2024@users.noreply.github.com>
2024-03-11 17:27:41 +0530

    Update score.js

--- score.js
+++ score.js
@@ -1,1 +1,11 @@
 // Iteration 8: Making scoreboard functional
+const playAgainBtn = document.getElementById("play-again-button");
+const scoreBoard = document.getElementById("score-board");
+
+if (playAgainBtn)
+  playAgainBtn.onclick = function () {
+    window.location.href = "./index.html";
+  };
+
+// Iteration 5: Store the player score and display it on the game over screen
+if (scoreBoard) scoreBoard.innerText = localStorage.getItem("score");
